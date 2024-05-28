01e1c99610dfdfe22b379b9eb7f6ba44415c5172
nehank <nehankpoilkar.136@gmail.com>
2024-05-28 17:43:05 +0530

    initial commit

---
+++ app/src/main/java/com/example/nehank/starbucksappui_androidcompose/ui/theme/Color.kt
@@ -0,0 +1,106 @@
+package com.example.nehank.starbucksappui_androidcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Red
+val RedColor200 = Color(0xFFEF9A9A)
+val RedColor500 = Color(0xFFF44336)
+val RedColor700 = Color(0xFFD32F2F)
+
+// Pink
+val PinkColor200 = Color(0xFFF48FB1)
+val PinkColor500 = Color(0xFFE91E63)
+val PinkColor700 = Color(0xFFC2185B)
+
+// Purple
+val PurpleColor200 = Color(0xFFCE93D8)
+val PurpleColor500 = Color(0xFF9C27B0)
+val PurpleColor700 = Color(0xFF7B1FA2)
+
+// Deep Purple
+val DeepPurpleColor200 = Color(0xFFB39DDB)
+val DeepPurpleColor500 = Color(0xFF673AB7)
+val DeepPurpleColor700 = Color(0xFF512DA8)
+
+// Indigo
+val IndigoColor200 = Color(0xFF9FA8DA)
+val IndigoColor500 = Color(0xFF3F51B5)
+val IndigoColor700 = Color(0xFF303F9F)
+
+// Blue
+val BlueColor200 = Color(0xFF90CAF9)
+val BlueColor500 = Color(0xFF2196F3)
+val BlueColor700 = Color(0xFF1976D2)
+
+// Light Blue
+val LightBlueColor200 = Color(0xFF81D4FA)
+val LightBlueColor500 = Color(0xFF03A9F4)
+val LightBlueColor700 = Color(0xFF0288D1)
+
+// Cyan
+val CyanColor200 = Color(0xFF80DEEA)
+val CyanColor500 = Color(0xFF00BCD4)
+val CyanColor700 = Color(0xFF0097A7)
+
+// Teal
+val TealColor200 = Color(0xFF80CBC4)
+val TealColor500 = Color(0xFF009688)
+val TealColor700 = Color(0xFF00796B)
+
+// Green
+val GreenColor200 = Color(0xFFA5D6A7)
+val GreenColor500 = Color(0xFF4CAF50)
+val GreenColor700 = Color(0xFF388E3C)
+
+// Light Green
+val LightGreenColor200 = Color(0xFFC5E1A5)
+val LightGreenColor500 = Color(0xFF8BC34A)
+val LightGreenColor700 = Color(0xFF689F38)
+
+// Lime
+val LimeColor200 = Color(0xFFE6EE9C)
+val LimeColor500 = Color(0xFFCDDC39)
+val LimeColor700 = Color(0xFFAFB42B)
+
+// Yellow
+val YellowColor200 = Color(0xFFFFF59D)
+val YellowColor500 = Color(0xFFFFEB3B)
+val YellowColor700 = Color(0xFFFBC02D)
+
+// Amber
+val AmberColor200 = Color(0xFFFFE082)
+val AmberColor500 = Color(0xFFFFC107)
+val AmberColor700 = Color(0xFFFFA000)
+
+// Orange
+val OrangeColor200 = Color(0xFFFFCC80)
+val OrangeColor500 = Color(0xFFFF9800)
+val OrangeColor700 = Color(0xFFF57C00)
+
+// Deep Orange
+val DeepOrangeColor200 = Color(0xFFFFAB91)
+val DeepOrangeColor500 = Color(0xFFFF5722)
+val DeepOrangeColor700 = Color(0xFFE64A19)
+
+// Brown
+val BrownColor200 = Color(0xFFBCAAA4)
+val BrownColor500 = Color(0xFF795548)
+val BrownColor700 = Color(0xFF5D4037)
+
+// Gray
+val GrayColor200 = Color(0xFFEEEEEE)
+val GrayColor500 = Color(0xFF9E9E9E)
+val GrayColor700 = Color(0xFF616161)
+
+// Blue Gray
+val BlueGrayColor200 = Color(0xFFB0BEC5)
+val BlueGrayColor500 = Color(0xFF607D8B)
+val BlueGrayColor700 = Color(0xFF455A64)
